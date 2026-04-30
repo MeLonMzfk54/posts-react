8849af97ebe8a0ceee11699f86507fe4f3e050ad
Garifullin Anton <garifullin@paylate.info>
2026-04-30 15:13:25 +0700

    feat: posts skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,28 @@ function App() {
     <main className="main">
       <div className='container'>
         <h2 className='main-title'>{title}</h2>
-        <p>Здесь будет контент блин</p>
+
+        <input type="text" placeholder='Поиск поста' className='search-input' />
+
+        <section className='posts'>
+          <article className='post-card'>
+            <h3>Первый пост</h3>
+            <p>Описание поста.</p>
+            <a href="#">Открыть пост</a>
+          </article>
+
+          <article className='post-card'>
+            <h3>Второй пост</h3>
+            <p>Описание поста.</p>
+            <a href="#">Открыть пост</a>
+          </article>
+
+          <article className='post-card'>
+            <h3>Третий пост</h3>
+            <p>Описание поста.</p>
+            <a href="#">Открыть пост</a>
+          </article>
+        </section>
       </div>
     </main>
   </div>
