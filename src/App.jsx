22d746359c42a5411d68d3a0f0ff78e4b9934045
Garifullin Anton <garifullin@paylate.info>
2026-04-30 15:03:32 +0700

    feat: header + main styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
 
 function App() {
+  const title = 'Добро пожаловать сюда'
 
-  return (
-      <h1>Hello</h1>
-  )
+  return <div className="app">
+    <header className="header">
+      <div className="container header-inner">
+        <h1 className='logo'>Logo</h1>
+        <nav className="nav">
+          <a href="#">Home</a>
+          <a href="#">Posts</a>
+          <a href="#">About</a>
+          <a href="#">Submit posts</a>
+        </nav>
+        <button>Dark Mode</button>
+      </div>
+    </header>
+
+    <main className="main">
+      <div className='container'>
+        <h2 className='main-title'>{title}</h2>
+        <p>Здесь будет контент блин</p>
+      </div>
+    </main>
+  </div>
 }
 
 export default App
